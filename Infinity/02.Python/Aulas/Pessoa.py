1da3fa55a571177488e5380603f6e4c8ed889b72
André Brito <andrebrito.analista@gmail.com>
2022-09-18 10:28:46 -0300

    Prova de Função

--- Infinity/02.Python/Aulas/Pessoa.py
+++ Infinity/02.Python/Aulas/Pessoa.py
@@ -4,12 +4,6 @@ class Pessoa:
         self.__sobrenome = ''
         self.__nascimento = '01/01/2022'
         
-    def getNome(self):
-        return self.__nome
-    
-    def setNome(self, nome):
-        self.__nome = nome     
-        
     @property
     def nome(self):
         return self.__nome
